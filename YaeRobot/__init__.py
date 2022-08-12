96b42c4e73879ee2142d93cbc85ab7224030e297
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-08-12 17:50:43 +0530

    Update __init__.py

--- YaeRobot/__init__.py
+++ YaeRobot/__init__.py
@@ -280,7 +280,7 @@ tg.RegexHandler = CustomRegexHandler
 tg.CommandHandler = CustomCommandHandler
 tg.MessageHandler = CustomMessageHandler
 
-REDIS_URL = "redis-17106.c15.us-east-1-2.ec2.cloud.redislabs.com:17106"
+REDIS_URL = "redis://xelcius:Xelcius~97@redis-11262.c301.ap-south-1-1.ec2.cloud.redislabs.com:11262"
 
 
 REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
