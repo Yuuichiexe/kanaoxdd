2b53b5e21f0be777f29ba30b74c5e2149ba080b7
Rishu <115208147+XoRishu@users.noreply.github.com>
2023-03-24 16:14:51 +0530

    Update __init__.py

--- YaeRobot/__init__.py
+++ YaeRobot/__init__.py
@@ -280,7 +280,7 @@ tg.RegexHandler = CustomRegexHandler
 tg.CommandHandler = CustomCommandHandler
 tg.MessageHandler = CustomMessageHandler
 
-REDIS_URL = "redis://:dwOWU5niSuu2noWTnc47kX0f39U8wvZd@redis-12168.c246.us-east-1-4.ec2.cloud.redislabs.com:12168"
+REDIS_URL = "redis://xelcius:Xelcius~97@redis-11612.c240.us-east-1-3.ec2.cloud.redislabs.com:11612"
 
 
 REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
