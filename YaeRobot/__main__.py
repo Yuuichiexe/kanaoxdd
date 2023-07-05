8d8cbd5b31edd52ed9e0f3b08a33449285815fb9
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-07-05 11:26:34 +0530

    Update __main__.py

--- YaeRobot/__main__.py
+++ YaeRobot/__main__.py
@@ -240,7 +240,7 @@ def start(update: Update, context: CallbackContext):
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
             )
-        else:
+          else:
         
         first_name = update.effective_user.first_name
         update.effective_message.reply_photo(
